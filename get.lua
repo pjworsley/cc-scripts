50e785c51e0e2caadd753d12be6f512182994e4b
Peter W <peterjamesworsley@gmail.com>
2020-12-31 13:16:44 +0000

    make amount argument number

--- get.lua
+++ get.lua
@@ -3,7 +3,7 @@ local storage = require("storage")
 destination = "right"
 
 item_name = arg[1]
-amount = arg[2] or 64
+amount = tonumber(arg[2]) or 64
 
 success = storage.move_item(item_name, amount, destination)
 print(success)
