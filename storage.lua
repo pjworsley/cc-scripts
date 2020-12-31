fde85a371edfd7b06210a045a29cb544ae3630f6
Peter W <peterjamesworsley@gmail.com>
2020-12-31 11:15:11 +0000

    Initial commit of storage functions

---
+++ storage.lua
@@ -0,0 +1,23 @@
+storage_type = "rftoolsstorage:modular_storage"
+
+function query_storage(item_name)
+    inventories = {peripheral.find(storage_type)};
+    entries = {};
+    for _, inv in ipairs(inventories) do
+        for _, item in ipairs(inv.list()) do
+            if item["name"] == item_name then
+                -- append entry to thej entries array
+                entries[#entries + 1] = item;
+            end
+        end
+    end
+    return entries;
+end
+
+function total_items(items)
+    total = 0;
+    for _ , stack in ipairs(items) do
+        total = total + items[i]["count"];
+    end
+    return total;
+end
