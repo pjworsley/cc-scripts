34e19102148fd56dae54cc8c8917331e91c54e5c
Peter W <peterjamesworsley@gmail.com>
2021-01-03 15:43:44 +0000

    Updated logging for me client

--- me_transfer/me_client.lua
+++ me_transfer/me_client.lua
@@ -14,7 +14,7 @@ function meclient.request(item_name, amount)
     rednet.open(peripheral.getName(MODEM))
     online = ping_server()
     if not online then
-        print("Controller is offline!")
+        print("ME Server is offline!")
         return false
     end
     rednet.send(SERVER, item_name .. " " .. amount, PROTOCOL)
