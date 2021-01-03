22fe6df9749b5f7a653c3dfd6fcdef4f4cf6eaaf
Peter W <peterjamesworsley@gmail.com>
2021-01-03 15:31:57 +0000

    update chest map

--- ore_processing/chestmap.lua
+++ ore_processing/chestmap.lua
@@ -1,7 +1,19 @@
 map = {
     pete = {
-        ore_chest = 14,
-        ingot_chest = 15
+        ore_chest = 31,
+        ingot_chest = 9
+    },
+    connor = {
+        ore_chest = 32,
+        ingot_chest = 5
+    },
+    chris = {
+        ore_chest = 33,
+        ingot_chest = 11
+    },
+    sam = {
+        ore_chest = 34,
+        ingot_chest = 13
     }
 }
 
