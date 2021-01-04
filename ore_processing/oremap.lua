748ea81f6ae9b169f5beb109b4617131dac80d0a
Peter W <peterjamesworsley@gmail.com>
2021-01-04 21:34:12 +0000

    Update ore map

--- ore_processing/oremap.lua
+++ ore_processing/oremap.lua
@@ -1,4 +1,17 @@
 local ore_ingot_map = {}
 ore_ingot_map["minecraft:iron_ore"] = "minecraft:iron_ingot"
+ore_ingot_map["minecraft:gold_ore"] = "minecraft:gold_ingot"
+ore_ingot_map["alltheores:ore_aluminum"] = "alltheores:aluminium_ingot"
+ore_ingot_map["alltheores:ore_copper"] = "alltheores:copper_ingot"
+ore_ingot_map["alltheores:ore_nickel"] = "alltheores:nickel_ingot"
+ore_ingot_map["alltheores:ore_osmium"] = "alltheores:osmium_ingot"
+ore_ingot_map["alltheores:ore_lead"] = "alltheores:lead_ingot"
+ore_ingot_map["alltheores:ore_platinum"] = "alltheores:platinum_ingot"
+ore_ingot_map["alltheores:ore_silver"] = "alltheores:silver_ingot"
+ore_ingot_map["alltheores:ore_tin"] = "alltheores:tin_ingot"
+ore_ingot_map["alltheores:ore_zinc"] = "alltheores:zinc_ingot"
+ore_ingot_map["alltheores:ore_uranium"] = "alltheores:uranium_ingot"
+ore_ingot_map["allthemodium:allthemodium_ore"] = "allthemodium:allthemodium_ingot"
+ore_ingot_map["allthemodium:unobtainium_ore"] = "allthemodium:unobtainium_ingot"
 
 return ore_ingot_map
